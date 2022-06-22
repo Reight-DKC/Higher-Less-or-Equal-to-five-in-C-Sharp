4263e8acc7d7b213abc5a2b4109e2d56c3b8c8e9
Reight-DKC <reightultimate@gmail.com>
2022-06-21 20:20:56 -0600

    Equal, Higher or Lower to 5 homework complete

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string input = Console.ReadLine();
+int number = Int32.Parse(input);
+
+int module = number;
+
+Console.WriteLine(module);
+
+if (module > 5)
+{
+Console.WriteLine("Higher than five");
+}  
+else if (module < 5)
+{
+    Console.WriteLine("Less than five");
+    }
+    else if (module == 5)
+    {
+        Console.WriteLine("Equal to five");
+    }
+
+Console.WriteLine("\nProgram ended");
+  
